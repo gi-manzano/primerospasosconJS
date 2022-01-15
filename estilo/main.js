b1ad795019c062ec87ab76725fa36f5470747276
Gisele <manzanogisele85@gmail.com>
2022-01-15 15:30:10 -0300

    utilizando js

---
+++ estilo/main.js
@@ -0,0 +1,3 @@
+/* tipos de datos: undefined (cuando no existe valor asignado), null (nulo-objeto que no extiste), boolean (boleano valores como verdadero o falso),string (tipo de datos para representar cadena de caracteres),symbol (), number (para representar numero en nuestro codigo), y object(propiedad que nos va a permitir relacionar funciones ..) */
+var miVariable= "miClaseJs";
+console.log (miVariable);
