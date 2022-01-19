2f2cdff1ce1603b0515bd9b54003454b56520faa
Gisele <manzanogisele85@gmail.com>
2022-01-19 12:59:04 -0300

    js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 
 // ciclos en bucles utilizando for (desde; hasta; actualizacion)//
 
-for (let i = 1; i <= 20; i++){   
+for (let i = 0; i <= 20; i++){   
     
     if (i == 5)
     {continue;}
